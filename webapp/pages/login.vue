f44e4462a95ab0ba0540b6fd67efb1178a5181d4
sayako21 <>
2024-10-20 08:43:37 +0900

    [追加]ツールチップの調整

--- webapp/pages/login.vue
+++ webapp/pages/login.vue
@@ -20,15 +20,11 @@ useHead({
     class="mx-auto min-h-[300px] mt-10 flex flex-col justify-center w-full max-w-md px-4 py-8 bg-white rounded-lg shadow sm:px-6 md:px-8 lg:px-10"
   >
     <div class="flex items-center justify-center px-2 mb-3 mx-auto">
-      <!--<img src="/img/kajitter.png" alt="" />-->
       <blur-reveal :delay="0.2" :duration="0.75">
-        <h2
-          class="text-3xl text-center p-2 font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-        >
-          CODE DNA 🧬
-        </h2>
+        <img src="/CodeDNA.PNG" alt="CODE DNA 🧬" />
+
         <p class="font-light text-gray-600 p-3">
-          GitHubのコードデータを使って、あなたの「コーディングDNA」を解析します。
+          GitHubのコードデータを使って、あなたの「コーディングDNA」を解析し、エンジニアアイデンティティを明らかにします。
         </p>
       </blur-reveal>
     </div>
