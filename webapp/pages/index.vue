833e249f433271ff497c8e8d5acbe7102c0a7057
sey323 <sey323_nm@yahoo.co.jp>
2024-10-19 23:50:28 +0900

    :+1: 処理中の時に処理中の画面を表示するガイドを表示

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -5,6 +5,7 @@ const client = useSupabaseClient();
 const userId = ref("");
 const userEmail = ref("");
 const analyticsData = ref({});
+const analyticsStatus = ref(DnaSummaryCreateStatus.NotYet);
 
 onMounted(async () => {
   const user = await client.auth.getUser();
@@ -19,11 +20,38 @@ onMounted(async () => {
   userId.value = user.data.user?.id!;
   userEmail.value = user.data.user?.email!; // ユーザーのメールアドレスを取得
 
-  analyticsData.value = await fetchAnalysisData();
+  analyticsStatus.value = await fetchAnalysisStatus();
+  console.log(analyticsStatus.value);
+
+  if (analyticsStatus.value === DnaSummaryCreateStatus.Completed) {
+    // 診断が未実施の場合は、診断を実行
+    analyticsData.value = await fetchAnalysisData();
+  }
 
   // TODO: 暫定処理でログインと同時に取得する。本来はユーザが「診断する」ボタンをクリックした時に実行する
   await syncGithubProviderTokenAndUpdateStatus();
 });
+
+const fetchAnalysisStatus = async () => {
+  const { data, error } = await client
+    .from("profiles")
+    .select("dna_summary_create_status")
+    .eq("id", userId.value);
+
+  if (error) {
+    console.error(error);
+    return DnaSummaryCreateStatus.NotYet;
+  }
+
+  if (data.length === 0) {
+    // プロフィールが存在しない場合は新規作成
+    return DnaSummaryCreateStatus.NotYet;
+  }
+
+  // enumのDnaSummaryCreateStatusに変換
+  const status = data[0].dna_summary_create_status;
+  return status;
+};
 
 
 const fetchAnalysisData = async () => {
@@ -49,29 +77,7 @@ const fetchAnalysisData = async () => {
 const syncGithubProviderTokenAndUpdateStatus = async () => {
   // Githubのアクセストークンを取得
   const session = await client.auth.getSession();
-  const userName = session.data.session?.user.user_metadata.user_name;
   const providerToken = session.data.session?.provider_token;
-
-  const response = await fetch(
-    `https://api.github.com/users/${userName}/events`,
-    {
-      // {username}にGitHubのユーザー名を入れます
-      headers: {
-        Authorization: `token ${providerToken}`,
-      },
-    }
-  );
-
-  // Githubのアクセストークン向こうのレスポンスが401の場合は、アクセストークンが無効なので再認証を促す
-  if (response.status === 401) {
-    // 401 Unauthorizedの場合は、アクセストークンが無効なので、再認証を促す
-    alert("GitHubのアクセストークンが無効です。再認証してください。");
-    client.auth.signOut();
-    window.location.href = "/login";
-  } else {
-    // デバッグ用にレスポンスをコンソールに表示
-    console.log(response);
-  }
 
   // DBに更新リクエストを登録
   const { data, error } = await client
@@ -115,7 +121,14 @@ const syncGithubProviderTokenAndUpdateStatus = async () => {
           "
         ></div>
       </div>
-      <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
+      <!-- ログインしていないときはログイン画面 -->
+      <div 
+        v-if="analyticsStatus === DnaSummaryCreateStatus.InProgress"
+        class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32"
+      >
+        処理中の画面を出す
+      </div>
+      <div v-else class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32">
         <div class="text-center">
           <p>{{ userEmail }}さんは</p>
           <h1
--- webapp/types/dna_summary_create_status.ts
+++ webapp/types/dna_summary_create_status.ts
@@ -1,5 +1,5 @@
 enum DnaSummaryCreateStatus {
-  Never = 0, // まだ作成されていない状態
+  NotYet = 0, // まだ作成されていない状態
   InProgress = 1, // 作成中
   Completed = 2, // 作成完了
   Failed = 3, // 作成失敗
