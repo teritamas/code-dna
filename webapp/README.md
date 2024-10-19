3aa18805408778cf0bf7d5d368ea82d796ae441b
sey323 <sey323_nm@yahoo.co.jp>
2024-10-19 15:32:01 +0900

    :gear: supabaseのDBの設定

--- webapp/README.md
+++ webapp/README.md
@@ -73,3 +73,31 @@ bun run preview
 ```
 
 Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
+
+## 開発者向け
+
+### DB の操作
+
+DB は Supabase を使用しています。DB の操作を行うために、Supabase にログインし、プロジェクトを紐づける必要があります。
+
+```bash
+npx supabase login
+supabase link
+# アカウントに紐づいたプロジェクトが表示されるので、該当のプロジェクトを選択して紐づける
+```
+
+#### Supabase から DB のスキーマをエクスポート
+
+Supabase からスキーマをエクスポートし、`types/schema.ts` に保存する。
+
+```bash
+supabase gen types typescript --linked > ./types/schema.ts
+```
+
+#### Trigger の設定
+
+ユーザ新規作成後、`profiles` テーブルに新規レコードを追加するための Trigger を設定する必要がある。
+
+SQL は以下 SQL を実行する
+
+- [`sql/trigger.sql`](sql/trigger.sql)
---
+++ webapp/types/schema.ts
@@ -0,0 +1,213 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string
+          query?: string
+          variables?: Json
+          extensions?: Json
+        }
+        Returns: Json
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+  public: {
+    Tables: {
+      code_dna_summary: {
+        Row: {
+          commit_granularity_rate: number | null
+          created_at: string
+          id: number
+          identity_name: string | null
+          method_splitting_coarseness_rate: number | null
+          processing_intent_communicating_rate: number | null
+          profile_id: string | null
+          summary_comment: string | null
+          variable_name_simplicity_rate: number | null
+        }
+        Insert: {
+          commit_granularity_rate?: number | null
+          created_at?: string
+          id?: number
+          identity_name?: string | null
+          method_splitting_coarseness_rate?: number | null
+          processing_intent_communicating_rate?: number | null
+          profile_id?: string | null
+          summary_comment?: string | null
+          variable_name_simplicity_rate?: number | null
+        }
+        Update: {
+          commit_granularity_rate?: number | null
+          created_at?: string
+          id?: number
+          identity_name?: string | null
+          method_splitting_coarseness_rate?: number | null
+          processing_intent_communicating_rate?: number | null
+          profile_id?: string | null
+          summary_comment?: string | null
+          variable_name_simplicity_rate?: number | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "code_dna_summary_profile_id_fkey"
+            columns: ["profile_id"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          dna_summary_create_status: number | null
+          id: string
+          user_name: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          dna_summary_create_status?: number | null
+          id: string
+          user_name?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          dna_summary_create_status?: number | null
+          id?: string
+          user_name?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
