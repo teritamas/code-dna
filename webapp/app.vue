439dd8e14bd54fce79c18a3e825af70341907c3f
sayako21 <>
2024-10-19 18:40:14 +0900

    [追加]ログイン画面

--- webapp/app.vue
+++ webapp/app.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <NuxtPage />
+    <main>
+      <NuxtPage />
+    </main>
+    <app-footer class="app-footer" />
   </div>
 </template>
+
+<style scoped>
+main {
+  margin-top: 20px; /* ヘッダーの高さ分だけマージンを設定 */
+  margin-bottom: 60px; /* ヘッダーの高さ分だけマージンを設定 */
+  padding-top: 40px;
+  min-height: 88vh;
+}
+
+.app-footer {
+  position: fixed; /* 固定 */
+  bottom: 0; /* 下に固定 */
+  left: 0; /* 左に固定 */
+  width: 100vw; /* 幅を100vhに設定 */
+  background-color: white; /* 背景色を指定 */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* 影を追加 */
+  z-index: 20; /* 他の要素の上に表示 */
+}
+</style>
