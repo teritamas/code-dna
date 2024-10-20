eef777634af63381d305ad81a9919e9303d0fc44
sey323 <sey323_nm@yahoo.co.jp>
2024-10-20 11:20:19 +0900

    :+1: プロンプト修正とLLMのリクエストでエラーが出た時に更新ステータスを3にする

--- analytics-job/analyticsjob/apis/llm_api.py
+++ analytics-job/analyticsjob/apis/llm_api.py
@@ -9,9 +9,9 @@ def generate_text(prompt):
         response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[{"role": "user", "content": prompt}],
-            temperature=0.2,
+            temperature=0.4,
         )
         return response.choices[0].message.content
     except Exception as e:
         print(f"An error occurred: {e}")
-        return ""
+        raise e
